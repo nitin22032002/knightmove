f28d1363e49cb178ba00e7434b3ceeadc9f7af62
nitin22032002 <79556510+nitin22032002@users.noreply.github.com>
2022-04-02 14:26:06 +0530

    task 2 complete

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ChessBoard from "./Component/ChessBoard";
+function App() {
+  return (
+    <div>
+      <div className="main-heading">
+              Posible Knight Move's 
+      </div>
+      <ChessBoard/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/ChessBoard.js
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+import "../css/chessboard.css"
+export default function ChessBoard() {
+  return (
+    <div className='main-div'>
+       
+            <div className='main-chess-body'>
+                <GenerateChessBlock/>
+            </div>
+    </div>
+  )
+}
+
+function Range(start,end){
+    let result=[]
+    for(let i=start;i<=end;i++){
+        result.push(i)
+    }
+    return result;
+}
+
+function GenerateChessBlock(){
+    let arr=Range(1,8)
+    const [getPosible,setPosible]=useState(JSON.stringify([]))
+    const [getSelected,setSelected]=useState({i:"",j:""})
+    const handleClick=(i,j,e)=>{
+        setSelected({i,j})
+        let possiblePosition=[`${i+2},${j-1}`,`${i+2},${j+1}`,`${i-2},${j-1}`,`${i-2},${j+1}`,`${i+1},${j-2}`,`${i-1},${j-2}`,`${i+1},${j+2}`,`${i-1},${j+2}`]
+        setPosible(JSON.stringify(possiblePosition))        
+    }
+    return(
+        <div className='vertical'>
+            {
+                arr.map((i)=>{
+                    return(
+                       <div className='horizontal'>
+                        {arr.map((j)=>{
+                            return(
+                                <div className={`block ${(parseInt(getSelected.i)===i && parseInt(getSelected.j)===j)?"selected":JSON.parse(getPosible).includes(`${i},${j}`)?"possible":""}`}  onClick={(e)=>{handleClick(i,j,e)}}>{(parseInt(getSelected.i)===i && parseInt(getSelected.j)===j)?"S":JSON.parse(getPosible).includes(`${i},${j}`)?"P":""}</div>
+                            )
+                        })}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
